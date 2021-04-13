08914112717154d12265363ef74429fcba8d345b
Vivianne Cardillo <viviannecardillo@Viviannes-MBP.fios-router.home>
2021-04-12 20:24:54 -0400

    added db connection code for googlebooks, created book schema/controller

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
-const express = require("express");
+const express = require("express"); //
 const path = require("path");
-const PORT = process.env.PORT || 3001;
-const app = express();
+const PORT = process.env.PORT || 3001;  //
+const app = express(); //
 
+
+//
+// Include express middleware 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Add routes for both api and view
+app.use(routes);
+
+//
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -14,6 +24,14 @@ app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
+
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+
+
+
+// start the api server
 app.listen(PORT, function() {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+  console.log(`🌎 ==> API server now listening on port ${PORT}!`);
 });
