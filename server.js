d021b3e479bace70f383733d1b85a8cff13f6d26
Vivianne Cardillo <viviannecardillo@Viviannes-MBP.fios-router.home>
2021-04-14 15:27:59 -0400

    trying to deploy to heroku

--- server.js
+++ server.js
@@ -8,9 +8,10 @@ const PORT = process.env.PORT || 3001;
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 }
 // Add routes, both API and view
 app.use(routes);
